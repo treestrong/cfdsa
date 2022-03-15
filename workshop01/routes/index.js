70f6eec1f08f39484ff2fee4509bc8e523d2f73a
root <root@ip-172-31-3-216.ap-southeast-1.compute.internal>
2022-03-15 14:44:31 +0000

    update to workshop01 - include image and randomise statement

--- workshop01/routes/index.js
+++ workshop01/routes/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const stringArray = ["abc","xyz"]
-
+const stringArray = [
+  	"There are 10 kinds of people. Those who knows binary and those who don\’t",
+	"Logic will get you from A to B. Imagination will take you everywhere.",
+	"There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+	"It\’s not that I\’m so smart, it\’s just that I stay with problems longer",
+	"It is pitch dark. You are likely to be eaten by a grue."
+	]
+var statement = stringArray[0];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: stringArray[0]});
-});
 
+  statement = stringArray[Math.floor(Math.random()*5)];
+  console.log(statement);
+
+  res.render('index', { title: statement});
+});
 module.exports = router;
